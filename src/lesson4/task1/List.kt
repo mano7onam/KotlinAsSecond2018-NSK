3b639d6145a4fb9754baa8480ca076fb00bfafe2
Andrey <amatveev@ccfit.nsu.ru>
2018-02-28 18:34:01 +0700

    Some new in List

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -174,7 +174,17 @@ fun accumulate(list: MutableList<Double>): MutableList<Double> {
  * Результат разложения вернуть в виде списка множителей, например 75 -> (3, 5, 5).
  * Множители в списке должны располагаться по возрастанию.
  */
-fun factorize(n: Int): List<Int> = TODO()
+fun factorize(n: Int): List<Int> {
+    var num = n
+    val res = arrayListOf<Int>()
+    for (i in 2 .. n) {
+        while (num % i == 0) {
+            res.add(i)
+            num /= i
+        }
+    }
+    return res
+}
 
 /**
  * Сложная
@@ -182,7 +192,8 @@ fun factorize(n: Int): List<Int> = TODO()
  * Разложить заданное натуральное число n > 1 на простые множители.
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  */
-fun factorizeToString(n: Int): String = TODO()
+fun factorizeToString(n: Int): String =
+        factorize(n).joinToString(separator = "*", prefix = "$n -> ")
 
 /**
  * Средняя
