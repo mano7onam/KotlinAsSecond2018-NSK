d43e7f9dc7698f8e68313e12aa05abf4b807fdb1
Andrey <amatveev@ccfit.nsu.ru>
2018-03-01 22:28:10 +0700

    List impossible

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -316,4 +316,151 @@ fun roman(n: Int): String {
  * Например, 375 = "триста семьдесят пять",
  * 23964 = "двадцать три тысячи девятьсот шестьдесят четыре"
  */
-fun russian(n: Int): String = TODO()
+
+fun russianHundreds(n: Int) : String {
+    assert(n < 10)
+    return when (n) {
+        1 -> "сто"
+        2 -> "двести"
+        3 -> "триста"
+        4 -> "четыреста"
+        5 -> "пятьсот"
+        6 -> "шестьсот"
+        7 -> "семьсот"
+        8 -> "восемьсот"
+        9 -> "девятьсот"
+        else -> "not check: $n"
+    }
+}
+
+fun russianDozens(n: Int) : String {
+    assert(n < 10)
+    return when (n) {
+        1 -> "десять"
+        2 -> "двадцать"
+        3 -> "тридцать"
+        4 -> "сорок"
+        5 -> "пятьдесят"
+        6 -> "шестьдесят"
+        7 -> "семьдесят"
+        8 -> "восемьдесят"
+        9 -> "девяносто"
+        else -> "not check: $n"
+    }
+}
+
+fun russianTenNineeen(n: Int) : String {
+    assert(n < 10)
+    return when (n) {
+        0 -> "десять"
+        1 -> "одиннадцать"
+        2 -> "двенадцать"
+        3 -> "тринадцать"
+        4 -> "четырнадцать"
+        5 -> "пятнадцать"
+        6 -> "шестнадцать"
+        7 -> "семнадцать"
+        8 -> "восемнадцать"
+        9 -> "девятнадцать"
+        else -> "not check: $n"
+    }
+}
+
+fun russianUnits(n: Int) : String {
+    assert(n < 10)
+    return when (n) {
+        1 -> "один"
+        2 -> "два"
+        3 -> "три"
+        4 -> "четыре"
+        5 -> "пять"
+        6 -> "шесть"
+        7 -> "семь"
+        8 -> "восемь"
+        9 -> "девять"
+        else -> ""
+    }
+}
+
+fun russianUnitsThousands(n: Int) : String {
+    assert(n < 10)
+    return when (n) {
+        1 -> "одна тысяча"
+        2 -> "две тысячи"
+        3 -> "три тысячи"
+        4 -> "четыре тысячи"
+        5 -> "пять тысяч"
+        6 -> "шесть тысяч"
+        7 -> "семь тысяч"
+        8 -> "восемь тысяч"
+        9 -> "девять тысяч"
+        else -> "тысяч"
+    }
+}
+
+fun russianDozensHundreds(n: Int) : String {
+    assert(n < 100)
+    if (n == 0) {
+        return ""
+    }
+    if (n < 10) {
+        return russianDozens(n)
+    }
+    if (n % 10 == 0) {
+        return russianHundreds(n / 10)
+    }
+    return russianHundreds(n / 10) + " " + russianDozens(n)
+}
+
+fun russianAllThousands(n: Int) : String {
+    assert(n < 1000)
+    if (n == 0) {
+        return ""
+    }
+    if (n < 10) {
+        return russianUnitsThousands(n)
+    }
+    if (n % 10 == 0) {
+        return russianDozensHundreds(n / 10) + " тысяч"
+    }
+    if (n % 100 in 10..19) {
+        return if (n < 100)
+            russianTenNineeen(n) + " тысяч"
+        else
+            russianHundreds(n) + " " + russianTenNineeen(n) + " тысяч"
+    }
+    return russianDozensHundreds(n / 10) + " " + russianUnitsThousands(n % 10)
+}
+
+fun russianUnitsDozensHundreds(n: Int) : String {
+    assert(n < 1000)
+    if (n == 0) {
+        return ""
+    }
+    if (n < 10) {
+        return russianUnits(n)
+    }
+    if (n % 10 == 0) {
+        return russianDozensHundreds(n / 10)
+    }
+    if (n % 100 in 10..19) {
+        return if (n < 100)
+            russianTenNineeen(n)
+        else
+            russianHundreds(n) + " " + russianTenNineeen(n)
+    }
+    return russianDozensHundreds(n / 10) + " " + russianUnits(n)
+}
+
+fun russian(n: Int): String {
+    if (n == 0) {
+        return "ноль"
+    }
+    if (n < 1000) {
+        return russianUnitsDozensHundreds(n)
+    }
+    if (n % 1000 == 0) {
+        return russianAllThousands(n / 1000)
+    }
+    return russianAllThousands(n / 1000) + " " + russianUnitsDozensHundreds(n)
+}
