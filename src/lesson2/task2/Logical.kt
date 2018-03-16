59929af3a06eca0d5364a3d7514dae9f76fea6e4
Andrey <amatveev@ccfit.nsu.ru>
2018-03-16 23:36:25 +0700

    Fixed remarks Logical

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -39,7 +39,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
                  x2: Double, y2: Double, r2: Double): Boolean =
-        (Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) + r1 <= r2 + 0.0000001)
+        (Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) + r1 <= r2)
 
 /**
  * Средняя
